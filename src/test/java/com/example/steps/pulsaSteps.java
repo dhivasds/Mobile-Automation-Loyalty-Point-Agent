9df214001c7737f65971e9af9982f8a38728e8ca
dhivasds <dhivasdharma@gmail.com>
2022-07-08 15:55:36 +0700

    add features pulsa

---
+++ src/test/java/com/example/steps/pulsaSteps.java
@@ -0,0 +1,35 @@
+package com.example.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class pulsaSteps {
+    @When("I click button pulsa")
+    public void iClickButtonPulsa() {
+    }
+
+    @And("I input field nomer telepon")
+    public void iInputFieldNomerTelepon() {
+    }
+
+    @And("I choose operator")
+    public void iChooseOperator() {
+    }
+
+    @And("I click provider")
+    public void iClickProvider() {
+    }
+
+    @And("I click pulsa nominal")
+    public void iClickPulsaNominal() {
+    }
+
+    @And("I click lanjut")
+    public void iClickLanjut() {
+    }
+
+    @Then("I get result Buy Pulsa Success")
+    public void iGetResultBuyPulsaSuccess() {
+    }
+}
